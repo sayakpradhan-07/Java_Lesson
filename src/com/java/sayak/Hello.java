839f4f710aa5b95b1b6245c28659e4e64ad94901
Sayak Pradhan <sayakpradhan@Sayaks-MacBook-Air.local>
2024-07-26 18:58:50 +0530

    Day 1

---
+++ src/com/java/sayak/Hello.java
@@ -0,0 +1,15 @@
+package com.java.sayak;
+
+public class Hello {          //this is a block
+    public static void main(String[] args) {     // main method
+        System.out.println("Hello, World!");
+        System.out.println(8+9);
+        System.out.println(71-19);
+
+        int age = 26;
+        String secondName = "Pradhan";
+        System.out.println(secondName);
+
+        // int, float, char, boolean, byte, short, long, double  ---> PRIMITIVE(Basic) DATATYPE
+    }
+}
