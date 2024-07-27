b086d40c3867ed72c81d2722ee01c18029fabd56
Sayak Pradhan <sayakpradhan@Sayaks-MacBook-Air.local>
2024-07-28 01:10:57 +0530

    Day 3

---
+++ src/com/java/sayak/PrimeNumber.java
@@ -0,0 +1,31 @@
+package com.java.sayak;
+
+import java.util.Scanner;
+
+
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if(isPrime(number)) {
+            System.out.println(number + " is a prime number");
+        }
+        else {
+            System.out.println(number + " is not a prime number");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        for(int i=2; i<=num/2; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
